9f6d737ed50c000a7d941d17fb0f6373dee73def
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-05 00:36:47 -0700

    added test suite

---
+++ tests/test_git/test_simple.py
@@ -0,0 +1,13 @@
+import unittest
+
+from pyggc.git.simple import clone, get_num_commits
+
+
+class Test__clone(unittest.TestCase):
+
+    def test_(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
