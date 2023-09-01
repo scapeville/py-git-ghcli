dde72373c43fa18eff84a064bc5faf4629360487
Nicholas Valentinus <nvfastplease@gmail.com>
2023-09-01 08:16:56 -0700

    when asserting/handling error, dont rely on error message as it might change for different software version

--- pyggc/git/simple.py
+++ pyggc/git/simple.py
@@ -1,3 +1,4 @@
+import os as _os
 import subprocess as _sp
 
 
@@ -30,16 +31,13 @@ def get_num_commits(repo_root_dir:str, *, git_bin:str='git') -> int:
     ## Exceptions
     - `ValueError`: If `repo_root_dir` isn't a Git repo
     """
+    
+    ## Not a Git repo
+    if '.git' not in _os.listdir(repo_root_dir): raise ValueError(f'Dir {repr(repo_root_dir)} is not a git repo.')
+
     try:
         cmd = [git_bin, 'rev-list', '--count', 'HEAD']
         res = _sp.check_output(cmd, cwd=repo_root_dir, text=True, stderr=_sp.STDOUT)
         return int(res.strip())
-    except _sp.CalledProcessError as err:
-
-        ## Not a Git repo
-        if str(err).strip() == 'fatal: not a git repository (or any of the parent directories): .git':
-            raise ValueError(f'Dir {repr(repo_root_dir)} is not a git repo.')
-        
-        ## Git repo with no commits
-        if str(err).startswith("fatal: ambiguous argument 'HEAD': unknown revision or path not in the working tree."):
-            return 0
+    except _sp.CalledProcessError:  # Git repo with no commits
+        return 0
